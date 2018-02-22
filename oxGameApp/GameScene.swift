6a8dd1bf72318c3efb31d37a9a2b18a01f7a160e
KazukiShimamoto <jakky0529@gmail.com>
2018-02-22 21:18:03 +0900

    make init

--- oxGameApp/GameScene.swift
+++ oxGameApp/GameScene.swift
@@ -13,4 +13,19 @@ class GameScene: SKScene {
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
+    
+    required init(coder aDecoder: NSCoder){
+        fatalError("NSCoder not supported")
+    }
+    
+    override init(size: CGSize){
+        super.init(size: size)
+        
+        anchorPoint = CGPoint(x: 0, y: 0)
+        
+        let background = SKSpriteNode(imageNamed: "bg")
+        background.position = CGPoint(x: 0, y: 0)
+        background.anchorPoint = CGPoint(x: 0, y: 0)
+        addChild(background)
+    }
 }
