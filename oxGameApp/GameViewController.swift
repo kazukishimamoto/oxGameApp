59380b4dbac57c138465290c356a66cc9e60370e
KazukiShimamoto <jakky0529@gmail.com>
2018-02-22 22:10:10 +0900

    call scene.mark

--- oxGameApp/GameViewController.swift
+++ oxGameApp/GameViewController.swift
@@ -8,15 +8,12 @@
 
 import UIKit
 import SpriteKit
-import GameplayKit
 
 class GameViewController: UIViewController, UIGestureRecognizerDelegate {
-
     var scene: GameScene!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         let skView = view as! SKView
         skView.isMultipleTouchEnabled = false
         
@@ -32,5 +29,6 @@ class GameViewController: UIViewController, UIGestureRecognizerDelegate {
     @IBAction func didTap(_ sender: UITapGestureRecognizer) {
         let location = sender.location(in: view)
         print("tapped at \(location.x), \(location.y) \n")
+        scene.mark(location: location)
     }
 }
